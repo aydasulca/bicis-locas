fc4237a2d58843cfd9c6ad0409d48a0d05379129
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-03-02 17:39:15 -0500

    bicis-locas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+  var letras = /[A-Z]/
+  var titulo = document.getElementById('form-signup-heading');
+  var nombre = document.getElementById('name');
+  var apellido = document.getElementById('lastname');
+  var correo = document.getElementById('input-email');
+  var contrasena = document.getElementById('input-password');
+  var selecciona = document.getElementById('form-group');
+  var social = document.getElementById('input-social');
+  var recordar = document.getElementsByTagName('remember-me');
+  var boton = document.getElementsByClassName('btn');
+
+
 }
