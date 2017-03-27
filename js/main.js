72f0a4d0f19960384b054a76c1a0666ee3d65a7a
Ayda <suni_yuri_94@hotmail.com>
2017-03-27 00:17:25 -0500

    nuevoCambio

--- js/main.js
+++ js/main.js
@@ -5,36 +5,42 @@ function validateForm(){
   var apellido = document.getElementById('lastname').value;
   var correo = document.getElementById('input-email').value;
   var contrasena = document.getElementById('input-password').value;
-  var seleccion = document.getElementsByClassName('form-control').value;
-  var social = document.getElementById('input-social').value;
-  var recordar = document.getElementsByTagName('label').value;
-  var boton = document.getElementsByClassName('btn');
+  var seleccion = document.getElementById('bici').value;
   var letra = /[a-zA-Z]/;
   var mayuscula = /[A-Z]{1}/;
   var corr = /\S+@\S+\.\S+/
 //validando espacios
   if(nombre !="" && apellido!="" && correo!="" && contrasena!=""){
 //validando letras
-    if(letra.test(nombre) != true && letra.test(apellido) != true){
-      alert("Complete datos validos en los campos");
+    if(letra.test(nombre) != true){
+      alert("Estructura de nombre no válida");
+    }
+
+    if(letra.test(apellido) != true){
+      alert("Estructura de apellido no válida");
     }
 //validando mayusculas
-    if(mayuscula.test(nombre) != true && mayuscula.test(apellido) != true){
-      alert("Poner la primera letra de tus datos en mayuscula");
+    if(mayuscula.test(nombre) != true){
+      alert("Primera letra de tu nombre debe ser máyuscula");
+    }
+
+    if(mayuscula.test(apellido) != true){
+      alert("Primera letra de tu apellido debe ser máyuscula");
     }
 //validando correo
   if(corr.test(correo) != true){
-    alert("Completa bien tu correo");
+    alert("Estructura de correo no válida: \n Ingresar email@domain.com");
   }
   //validando campo password
   if(contrasena.toString().length < 6 || contrasena.toString() == "password" ||
       contrasena == "123456" || contrasena == "098754"){
-    alert("Contraseña no válida");
+    alert("Estructura de contraseña no válida: \n Ingresar contraseña de 6 caracteres");
     }
   //validando seleccion
-  if( bici == null || bici == 0 ) {
-	    alert("Completar datos en seleccionar bici");
-	  }
+  if(seleccion == "0"){
+    alert("Escoge tu tipo de bici");
+  }
+
   }else{
     alert("Completa todos los campos");
   }
